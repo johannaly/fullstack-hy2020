fd817194fbded242d6598271717eab71c9f4e8c7
johannaly <johanna@lyytinen.org>
2020-06-25 12:34:05 +0300

    puhelinluettelo 2.20

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,23 +6,35 @@ import PersonForm from './components/PersonForm'
 import personService from './services/persons'
 import Notification from './components/Notification'
 
-    
+
+
 const App = () => {
-    
+
     const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
-    const[newFilter, setNewFilter] = useState("")
+    const [newFilter, setNewFilter] = useState("")
     const [alertMessage, setAlertMessage] = useState(null)
+    const [isAlert, setAlert] = useState(false)
+
 
     useEffect(() => {
         personService
-        .getAll()
-        .then(persons => {
-            setPersons(persons)
-        })
+            .getAll()
+            .then(persons => {
+                setPersons(persons)
+            })
+            .catch(error => {
+                setAlert(true)
+                setAlertMessage(`Something went wrong`)
+                setTimeout(() => {
+                    setAlertMessage(null)
+                    setAlert(false)
+                }, 3000)
+            })
+
     }, [])
-    
+
 
     const handleNameChange = (event) => {
         setNewName(event.target.value)
@@ -35,60 +47,77 @@ const App = () => {
     const handleFilterChange = (event) => {
         setNewFilter(event.target.value)
     }
-    
+
     const addName = (event) => {
         event.preventDefault()
         const personObject = {
             name: newName,
             number: newNumber
         }
-        
-        if (persons.map(p=> p.name).includes(newName)) {
+
+        if (persons.map(p => p.name).includes(newName)) {
             const personData = persons.filter(p => p.name === newName)
             const personId = personData[0].id.toString()
-           // const testFind = (p) => p.id.toString() === personId 
-            const personIndex = persons.findIndex((p) => p.id.toString() === personId )
-            if(personData[0].number !== newNumber){  
+            // const testFind = (p) => p.id.toString() === personId 
+            const personIndex = persons.findIndex((p) => p.id.toString() === personId)
+            if (personData[0].number !== newNumber) {
                 window.confirm(`${newName} is already added to phonebook, replace
                 the old number with a new one?`)
                 personService
                     .updateNumber(personId, personObject)
-                    .then (person => {
-                           const newPersons = [...persons]
-                           newPersons[personIndex] = personObject
-                           setPersons(newPersons)      
+                    .then(person => {
+                        const newPersons = [...persons]
+                        newPersons[personIndex] = personObject
+                        setPersons(newPersons)
+                        setAlertMessage(`Number changed for ${newName}`)
+                        setTimeout(() => {
+                            setAlertMessage(null)
+
+                        }, 3000)
                     })
-                setAlertMessage(`Number changed for ${newName}`)
-                setTimeout(() => {
-                    setAlertMessage(null)
-                }, 3000)
-           } else {
-               window.alert(`${newName} is already added to phonebook`)
-           }
-        
+                    .catch(error => {
+                        setAlert(true)
+                        setAlertMessage(`Information of ${newName} has already been removed from server`)
+                        setTimeout(() => {
+                            setAlertMessage(null)
+                            setAlert(false)
+                        }, 3000)
+                    })
+
+            } else {
+                window.alert(`${newName} is already added to phonebook`)
+            }
+
         } else {
             personService
                 .create(personObject)
                 .then(person => {
                     setPersons(persons.concat(person))
+                    setAlertMessage(`Added ${newName}`)
+                    setTimeout(() => {
+                        setAlertMessage(null)
+                    }, 3000)
                 })
-            setAlertMessage(`Added ${newName}`)
-            setTimeout(() => {
-                setAlertMessage(null)
-            }, 3000)
+                .catch(error => {
+                    setAlert(true)
+                    setAlertMessage(`Something went wrong`)
+                    setTimeout(() => {
+                        setAlertMessage(null)
+                        setAlert(false)
+                    }, 3000)
+                })
         }
-        
         setNewName('')
         setNewNumber('')
     }
 
     const deleteOnePerson = (event) => {
-       
+
         if (window.confirm(`Delete ${event.target.value}?`)) {
             const personId = event.target.id.toString()
             personService
                 .deletePerson(event.target.id)
-                .then (data => {
+                .then(data => {
                     setPersons(persons.filter(p => p.id.toString() !== personId))
                 })
 
@@ -102,16 +131,16 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
-            <Notification message = {alertMessage} />
-            <SearchForm handleFilterChange = {handleFilterChange} />
+            <Notification message={alertMessage} isAlert={isAlert} />
+            <SearchForm handleFilterChange={handleFilterChange} />
 
             <h2>Add a new</h2>
-            <PersonForm addName = {addName} newName = {newName} newNumber = {newNumber}
-                handleNameChange = {handleNameChange} handleNumberChange = {handleNumberChange}
+            <PersonForm addName={addName} newName={newName} newNumber={newNumber}
+                handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}
             />
-            
+
             <h2>Numbers</h2>
-            <FilterNamesToShow persons = {persons} newFilter = {newFilter} deleteOnePerson = {deleteOnePerson} />
+            <FilterNamesToShow persons={persons} newFilter={newFilter} deleteOnePerson={deleteOnePerson} />
         </div>
     )
 }
--- osa2/puhelinluettelo/src/components/Notification.js
+++ osa2/puhelinluettelo/src/components/Notification.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
-const Notification = ({message}) => {
+const Notification = (props) => {
+    const message = props.message
+    const isAlert = props.isAlert
+
     if (message === null) {
         return null
     }
 
+    console.log({ isAlert })
+    const className = isAlert ? "error" : "notification"
     return (
-        <div className = "notification">
+        <div className={className}>
             {message}
         </div>
     )
